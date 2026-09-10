11b60ede155e8edcd6785336e16cfb9940dc0ec8
Сергей Андрианов <Tyuiu.AndrianovSA@outlook.com>
2026-09-10 14:58:27 +0500

    Task2

---
+++ Tyuiu.AndrianovSA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.AndrianovSA.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.AndrianovSA.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Сергей"));
+            Console.ReadKey();
+        }
+    }
+}
